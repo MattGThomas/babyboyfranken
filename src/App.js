d0a1827d541b8a2d63afe1bcd632e223237cd962
MattGThomas <mgthomas0616@gmail.com>
2020-05-18 01:17:50 -0500

    switched to react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>hello world</div>
+      <p>hello world</p>
+    </div>
+  );
+}
+
+export default App;
